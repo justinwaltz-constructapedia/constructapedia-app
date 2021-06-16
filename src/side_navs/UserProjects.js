9554737c92f24bf00849e3172fd5a19f35755fce
Zach <zachscoggins@constructapedia.com>
2021-06-16 10:24:36 -0500

    Button Color Change

--- src/side_navs/UserProjects.js
+++ src/side_navs/UserProjects.js
@@ -38,7 +38,7 @@ function UserProjects(props) {
                                     return (<li key={subPlan.title + i}>
                                                 <a href="#subPlan">
                                                     {subPlan.title}
-                                                    <i className="material-icons right"
+                                                    <i className="material-icons right blue-grey darken-4 blue-grey-text text-darken-3"
                                                         onClick={()=>deleteSubPlan(subPlan.title)}>delete_forever</i>
                                                 </a>
                                             </li>)
@@ -47,13 +47,14 @@ function UserProjects(props) {
                                     <li key={plan.id} className="bold">
                                         <div className="collapsible-header">
                                             <h6 className="valign-wrapper" onClick={()=>props.updateSelectedPlan(plan.id)}>{plan.title}<i className="material-icons">chevron_right</i></h6>
+                                            <button className="btn-flat waves-effect waves-light blue-grey darken-4 blue-grey-text text-lighten-5 right" onClick={()=>{props.removeUserPlan(plan.id)}}>
+                                                    <i className="material-icons blue-grey darken-4 blue-grey-text text-darken-3 center-align">delete_forever</i></button>
                                         </div>
                                         <div className="collapsible-body blue-grey darken-4 blue-grey-text text-lighten-5">
                                             <ul>
                                                 {subPlans}
                                                 <li>
-                                                    <button className="btn-flat waves-effect waves-light blue-grey darken-4 blue-grey-text text-lighten-5" onClick={()=>{props.removeUserPlan(plan.id)}}>
-                                                            <i className="material-icons left blue-grey darken-4 blue-grey-text text-darken-2">delete_forever</i></button>
+
                                                 </li>
                                             </ul>
                                         </div>
