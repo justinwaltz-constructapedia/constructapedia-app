26753993a1297bc781546c1cc502447c62060188
Justin Waltz <justinwaltz@constructapedia.com>
2021-07-20 16:48:08 -0500

    notes section working for new notes

--- src/main_views/notebook/NotesSection.js
+++ src/main_views/notebook/NotesSection.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 function NotesSection (props) {
-    const initialNoteValues = props.notes
+    const initialNotes = props.notes
     const [newNoteValue, setNewNoteValue] = useState("");
-    const [noteValues, setNoteValues] = useState(initialNoteValues);
+    const [noteValues, setNoteValues] = useState(initialNotes);
+
+    useEffect(() => {
+        setNoteValues(props.notes)
+    }, [props.notes])
 
     function saveChangesToNote (i) {
         console.log(i);
@@ -11,26 +15,21 @@ function NotesSection (props) {
     }
 
     const createNoteElement = (note, i) => (
-        <div key={note.contents + i} className="row">
-            <div className= "col s10 input-field">
-                <textarea
-                    className="materialize-textarea blue-grey darken-4 blue-grey-text text-lighten-5"
-                    value={note.contents}
-                    onChange={(e) => console.log(e.target.value)/*editNoteValue(e, i)*/}
-                    />
-            </div>
-            <div className= "col s2">
-                <button className="btn-floating waves-effect waves-light blue-grey darken-3 blue-grey-text text-lighten-5 left"
-                        type="button"
-                        //onClick={()=>saveChangesToNote(i)}
-                        >
-                    <i className="material-icons">save</i>
-                </button>
-            </div>
-        </div>
+        <Note
+            key={note.contents + i}
+            value={note.contents}
+            noteIndex={i}
+            changeValue={setNewNoteValue}/>
     )
 
-    const createNotes = () => props.notes.map(createNoteElement)
+    const createNotes = () => props.notes.map((note, i)=> {
+        return createNoteElement(note,i)
+    })
+
+    function saveNewNote () {
+        props.saveNotes(newNoteValue, -1)
+        setNewNoteValue("");
+    }
 
     return (
         <div className="row blue-grey darken-4 blue-grey-text text-lighten-5">
@@ -41,7 +40,7 @@ function NotesSection (props) {
                     <div className="col s1">
                         <button id={"add-note-btn-" + props.selectedPlanId}
                                 className="btn-floating btn-small waves-effect waves-light blue-grey darken-3 blue-grey-text text-lighten-5" type="button"
-                                onClick={() => props.saveNotes(newNoteValue, -1)}>
+                                onClick={saveNewNote}>
                                 <i className="material-icons">add</i>
                         </button>
                     </div>
@@ -51,12 +50,38 @@ function NotesSection (props) {
                                 onChange={(e) => setNewNoteValue(e.target.value)}
                                 onKeyDown={(e)=>{
                                     if(e.keyCode===13){
-                                        props.saveNotes(newNoteValue, -1)
+                                        saveNewNote()
                                     }
                                 }}
                                 placeholder={"Add New Note"}/>
                     </div>
                 </div>
+            </div>
+        </div>
+    )
+}
+
+function Note (props) {
+    const [noteValue, setNoteValue] = useState("");
+    useEffect(() => {
+        setNoteValue(props.value);
+    }, [props.value])
+    return (
+        <div className="row">
+            <div className= "col s10 input-field">
+                <textarea
+                    className="materialize-textarea blue-grey darken-4 blue-grey-text text-lighten-5"
+                    value={noteValue}
+                    onChange={(e) => console.log(e.target.value)/*editNoteValue(e, i)*/}
+                    />
+            </div>
+            <div className= "col s2">
+                <button className="btn-floating waves-effect waves-light blue-grey darken-3 blue-grey-text text-lighten-5 left"
+                        type="button"
+                        //onClick={()=>saveChangesToNote(i)}
+                        >
+                    <i className="material-icons">save</i>
+                </button>
             </div>
         </div>
     )
